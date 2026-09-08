c95be5c01db67d91ea24806dbece52815e675213
CHRIS KUO <chriskuo@CHRISs-Mac-mini.local>
2026-09-07 20:10:40 -0400

    remove US outlook, add japan, taiwan, southeast asia outlook

--- assets/js/config.js
+++ assets/js/config.js
@@ -8,8 +8,9 @@
 
 const SECTIONS = [
   {id:'sp500', label:'S&P 500 outlook'},
-  {id:'us', label:'US outlook'},
-  {id:'asia', label:'Asian outlook'},
+  {id:'japan', label:'Japan outlook'},
+  {id:'taiwan', label:'Taiwan outlook'},
+  {id:'sea', label:'Southeast Asia outlook'},
   {id:'tech', label:'Tech stocks outlook'},
   {id:'fedwatch', label:'Fed Watch'}
 ];
